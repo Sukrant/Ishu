10a9290d05713084d3849ba4835353b68de606fd
sukrant2tech@gmail.com <sukrant2tech@gmail.com>
2018-08-05 22:58:58 +0530

    commit 23 services.py changes for list of ports

--- monitoring/services/service_status.py
+++ monitoring/services/service_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import subprocess as sp
-service_name="mysql"
+service_name="http"
 
 def service_stat(service_name):
-	port=sp.Popen("netstat -ntlp| grep " + service_name + " | awk '{split($4,a,\":\")} {print a[length(a)]}'",stdout=sp.PIPE,shell=True).communicate()[0].rstrip()	
+	port=sp.Popen("netstat -ntlp| grep " + service_name + " | awk '{split($4,a,\":\")} {print a[length(a)]}'",stdout=sp.PIPE,shell=True).communicate()[0].rstrip().split("\n")	
 	if port:
 		print service_name ,"is running and Listening on TCP port : ", port
 	else:
