2dc0da1965753c753bd08e51955829e538970488
sukrant.sirohi <sukrant2tech@gmail.com>
2018-08-19 23:13:30 +0530

    httpd--> MySQL

--- monitoring/services/service_status.py
+++ monitoring/services/service_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import subprocess as sp
-service_name="http"
+service_name="mysqld"
 
 def service_stat(service_name):
 	port=sp.Popen("netstat -ntlp| grep " + service_name + " | awk '{split($4,a,\":\")} {print a[length(a)]}'",stdout=sp.PIPE,shell=True).communicate()[0].rstrip().split("\n")	
