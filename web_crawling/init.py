1c0206a697fede32eb253276e717769bd2494c2e
root <sukrant2tech@gmail.com>
2018-10-09 00:23:02 +0530

    requets/BeautifulSoup

--- web_crawling/init.py
+++ web_crawling/init.py
@@ -1,3 +1,18 @@
 /usr/bin/pythonm
 
 curl -s https://en.wikipedia.org/wiki/Main_Page| sed -n 's/.*href="\([^"]*\).*/\1/p'| grep ^http| sort -u
+
+
+>>> import requests
+>>> from bs4 import BeautifulSoup as bs
+>>> url=requests.get("http://www.google.com")
+>>> url.status_code
+200
+>>> url_content=url.content
+>>> type(url_content)
+<type 'str'>
+>>> url_soup = bs(url_content)
+>>> type(url_soup)
+<class 'bs4.BeautifulSoup'>
+>>> samples=url_soup.find_all("a", "item-title")
+>>> samples[0]
