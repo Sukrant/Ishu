88520cb1e651318c4e00678f7f56d01d347a6442
sukrant sirohi <sukrant2tech@gmail.com>
2019-10-07 22:18:22 +0100

    7-oct-2019-Monday

--- web_crawling/crawler.py
+++ web_crawling/crawler.py
@@ -1,25 +1,44 @@
+#!/usr/bin/python3.5 
+
 import requests
 from bs4 import BeautifulSoup as bs
 import datetime
-import MySQLdb
-import socket
+import mysql.connector 
+import sys
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="crawler",
+  passwd="passw0rd"
+)
 
-class crawler:
-    def __init__(self,link):
-        self.link=link
-        self.url=requests.get(self.link)
-        self.url_content=self.url.content
-        self.url_soup=bs(self.url_content)
-    def domain(self):
-        domain=self.link.split("//")[-1].split("/")[0]
-        return domain
-    def time(self):
-        time=datetime.datetime.now().strftime("%d:%m:%Y %H:%M:%S")
-        return time
-    def title(self):
-        title=str(self.url_soup.title.get_text())
-        return title
-    def urls(self):
-        urls=list(set([b for b in [a['href'] for a in self.url_soup.find_all('a', href=True) if a.text] if b.startswith('http')]))
-        return urls
+link=sys.argv[1]
+
+def link_url(link):
+    url=requests.get(link)
+    url_content=url.content
+    url_soup=bs(url_content,"html.parser")
+    return url_soup
+
+def domain(link):
+    domain=link.split("//")[-1].split("/")[0]
+    return domain
+
+def time():
+    time=datetime.datetime.now().strftime("%d:%m:%Y %H:%M:%S")
+    return time 
+
+def title(link):
+    url_soup=link_url(link)
+    title=str(url_soup.title.get_text())
+    return title
+
+def urls(url_soup):
+    urls=list(set([b for b in [a['href'] for a in url_soup.find_all('a', href=True) if a.text] if b.startswith('http')]))
+    return urls 
+
+if __name__ == "__main__":
+    print("Link : ",link)
+    print("Time :",time())
+    print("Domain :", domain(link))
+    print("Title :", title(link))
