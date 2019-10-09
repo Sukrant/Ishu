1f4291730585bbc1877e7771d09a562ef9c8ad54
ssirohi <sukrant2tech@gmail.com>
2019-10-09 22:06:42 +0100

    Simple-Query

--- web_crawling/crawler.py
+++ web_crawling/crawler.py
@@ -4,15 +4,20 @@ import requests
 from bs4 import BeautifulSoup as bs
 import datetime
 import mysql.connector 
+from  mysql.connector import Error
 import sys
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="crawler",
-  passwd="passw0rd"
-)
+try:
+    myconnection = mysql.connector.connect(
+        database = 'crawler',
+        host = 'localhost',
+        user = 'crawler',
+        password = 'passw0rd')
+except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table .. Exiting ... {}".format(error))
+        sys.exit(1)
 
-link=sys.argv[1]
+link = sys.argv[1]
 
 def link_url(link):
     url=requests.get(link)
@@ -34,11 +39,28 @@ def title(link):
     return title
 
 def urls(url_soup):
+    url_soup=link_url(link)
     urls=list(set([b for b in [a['href'] for a in url_soup.find_all('a', href=True) if a.text] if b.startswith('http')]))
     return urls 
+
+def link_data(link):
+    Domain=domain(link)
+    Title=title(link)
+    try:
+        cursor = myconnection.cursor()
+        link_query = """ insert into site_data (link,Domain,Title) values (%s, %s, %s) """
+        cursor.execute(link_query,(link,Domain,Title))
+        myconnection.commit()
+        print("Insert values for Link :", link)
+    except mysql.connector.Error as error:
+        print(" Because of MySQL insert error. Exit...")
+        sys.exit(2)
+    finally:
+        cursor.close()
 
 if __name__ == "__main__":
     print("Link : ",link)
     print("Time :",time())
     print("Domain :", domain(link))
     print("Title :", title(link))
+    link_data(link)
